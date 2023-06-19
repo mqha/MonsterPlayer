bc01b39c8e23182bb4a58eb4a9cc6a381f1173db
PC058\Admin <hunganh7793@gmail.com>
2023-06-19 21:05:15 +0700

    1906

---
+++ ConsoleApp1/Human.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Human
+    {
+        private int level;
+        private int health;
+        private int damage;
+        private int atkSpeed;
+        private int exp;
+
+        public int Level => level;
+        public int Health => health;
+        public int Damage => damage;
+        public int AttackSpeed => atkSpeed;
+        public int Exp => exp;
+
+
+        public Human()
+        {
+            this.level = 1;
+        }
+    }
+}
